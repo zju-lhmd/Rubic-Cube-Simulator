c69816a31b74bb1f95b3e6ee5da56c5b906c4e9a
zhongpang2023 <123851188+zhongpang2023@users.noreply.github.com>
2023-07-10 16:06:22 +0800

    VIEW层：处理输入

--- VIEW/Input.h
+++ VIEW/Input.h
@@ -9,6 +9,39 @@
 #include "../VIEWMODEL/CommandHandlers.h" // viewmodel
 
 class EventHandler {
+public:
+    EventHandler(CommandHandler* commandhandler = nullptr,
+                 GLFWwindow* window = nullptr,
+                 Renderer* renderer = nullptr);
+
+    ~EventHandler();
+
+    void SetWindow(GLFWwindow* window);
+    void SetCommandHandler(CommandHandler* commandhandler);
+    void SetRenderer(Renderer* renderer);
+
+    // event callback
+    void mouseButtonCallback(GLFWwindow*, int, int, int);// Callback for mouse button events
+    void keyboardCallback(GLFWwindow*, int, int, int, int);// Callback for keyboard events
+    void characterCallback(GLFWwindow*, unsigned int);// Callback for character input events
+    // state update
+    void mouseMove();
+    void keyboardScan();
+
+    std::string getCommand();
+private:
+    // execute command
+    void execCommand(std::string);
+    // reference
+    GLFWwindow* m_windowRef;
+    Renderer* m_rendererRef;
+    CommandHandler* m_commandHandlerRef;
+    // command buffer
+    std::string m_commandBuffer;// Buffer to store the entered command
+    // mouse state
+    double lastX, lastY;// Coordinates of the last mouse position
+    bool mouseDown;// Flag indicating if the mouse button is pressed
+    double lastAngleX, lastAngleY;// Last rotation angles
 };
 
 #endif
