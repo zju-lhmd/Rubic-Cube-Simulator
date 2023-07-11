ff66ba4e884c774eb666914eaf8d71085e391c77
lhmd <2683235973@qq.com>
2023-07-11 12:09:00 +0800

    完成common层命令基类

--- COMMON/Macro.h
+++ COMMON/Macro.h
@@ -24,4 +24,33 @@
 #define ROTATE_CLK(x)   MAKE_CUBE(GET_FRONT(x), GET_BACK(x), GET_DOWN(x), GET_UP(x), GET_LEFT(x), GET_RIGHT(x)) // clockwise
 #define ROTATE_CCLK(x)  MAKE_CUBE(GET_FRONT(x), GET_BACK(x), GET_UP(x), GET_DOWN(x), GET_RIGHT(x), GET_LEFT(x)) // counter-clockwise
 
+#define FL_EDGE 0][1][2 // front left edge
+#define FR_EDGE 2][1][2 // front right edge
+#define FU_EDGE 1][2][2 // front up edge
+#define FD_EDGE 1][0][2 // front down edge
+#define BL_EDGE 0][1][0 // back left edge
+#define BR_EDGE 2][1][0 // back right edge
+#define BU_EDGE 1][2][0 // back up edge
+#define BD_EDGE 1][0][0 // back down edge
+#define UL_EDGE 0][2][1 // up left edge
+#define UR_EDGE 2][2][1 // up right edge
+#define DL_EDGE 0][0][1 // down left edge
+#define DR_EDGE 2][0][1 // down right edge
+
+#define F_CENTRE 1][1][2 // front centre
+#define B_CENTRE 1][1][0 // back centre
+#define L_CENTRE 0][1][1 // left centre
+#define R_CENTRE 2][1][1 // right centre
+#define U_CENTRE 1][2][1 // up centre
+#define D_CENTRE 1][0][1 // down centre
+
+#define FLU_CORNER 0][2][2 // front left up corner
+#define FLD_CORNER 0][0][2 // front left down corner
+#define FRU_CORNER 2][2][2 // front right up corner
+#define FRD_CORNER 2][0][2 // front right down corner
+#define BLU_CORNER 0][2][0 // back left up corner
+#define BLD_CORNER 0][0][0 // back left down corner
+#define BRU_CORNER 2][2][0 // back right up corner
+#define BRD_CORNER 2][0][0 // back right down corner
+
 #endif
