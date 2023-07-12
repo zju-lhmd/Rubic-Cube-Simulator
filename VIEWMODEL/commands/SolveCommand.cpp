09bf87efd517b5ce0e743086fde3a6243263e5de
lhmd <2683235973@qq.com>
2023-07-12 11:27:34 +0800

    VIEWMODEL层：添加复原命令

--- VIEWMODEL/commands/SolveCommand.cpp
+++ VIEWMODEL/commands/SolveCommand.cpp
@@ -13,5 +13,18 @@ void SolveCommand::setParameter(const std::any& param)
 
 void SolveCommand::exec()
 {
-	return;
+	// stop any rotation right now
+	m_pVM->notifyAllObservers(0);
+	Cube cpyCube(*(m_pVM->m_pCube));
+	CubeSolver solver(cpyCube);
+	// solve cubes
+	solver.solve();
+	CubeSteps steps = solver.GetSteps();
+	// optimize steps
+	steps = ReduceFilter::Filter(steps);
+	steps = NoXYZFilter::Filter(steps);
+	steps = ReduceFilter::Filter(steps);
+	// start play steps
+	*m_pVM->m_pRSteps = steps;
+	m_pVM->notifyAllObservers(2);
 }
